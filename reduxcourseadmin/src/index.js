c8d96c381dcbe09bbddc0c7cc4a7be038fdd3770
Rakshith Bellamane <rakshith.bellamane@libertymutual.com>
2018-11-02 14:58:45 -0400

    inital course admin using redux

---
+++ reduxcourseadmin/src/index.js
@@ -0,0 +1,18 @@
+import 'babel-polyfill';
+import React from 'react';
+import { render } from 'react-dom';
+import configureStore from '../src/store/configureStore';
+import { Router, browserHistory} from 'react-router';
+import routes from './routes';
+import './styles/styles.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import {Provider} from 'react-redux';
+
+const store = configureStore();
+
+render(
+  <Provider store={store}>
+    <Router history={browserHistory} routes={routes}/>,
+  </Provider>,
+    document.getElementById('app')
+);
