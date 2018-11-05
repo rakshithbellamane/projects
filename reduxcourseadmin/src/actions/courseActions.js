b10403a4211f9d9491996ba0ce02d3e33ff9ba3f
Rakshith Bellamane <rakshith.bellamane@libertymutual.com>
2018-11-05 15:29:44 -0500

    add load initial course load

--- reduxcourseadmin/src/actions/courseActions.js
+++ reduxcourseadmin/src/actions/courseActions.js
@@ -1,4 +1,20 @@
 import * as types from '../actions/actionTypes';
-export function createCourse(course){
-  return ({type: types.CREATE_COURSE, course});
+import courseApi from '../api/mockCourseApi';
+
+function loadCoursesSuccess(courses){
+  return {
+    type: types.LOAD_COURSES_SUCCESS,
+    courses
+  };
+}
+
+export function loadCourses(){
+  return function(dispatch){
+    return courseApi.getAllCourses()
+      .then(
+        courses => {dispatch(loadCoursesSuccess(courses));}
+    ).catch(error => {
+      throw(error);
+    });
+  };
 }
--- reduxcourseadmin/src/index.js
+++ reduxcourseadmin/src/index.js
@@ -5,11 +5,13 @@ import configureStore from '../src/store/configureStore';
 import {Provider} from 'react-redux';
 import { Router, browserHistory} from 'react-router';
 import routes from './routes';
+import {loadCourses} from './actions/courseActions';
 import './styles/styles.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
-console.log('from main index.js');
 const store = configureStore();
+
+store.dispatch(loadCourses());
 
 render(
   <Provider store={store}>
