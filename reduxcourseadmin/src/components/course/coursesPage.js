6fe6dbd819aede10fb9dc695a8b42aaa03ff6f50
Rakshith Bellamane <rakshith.bellamane@libertymutual.com>
2018-11-05 12:57:03 -0500

    initial integration with redux

--- reduxcourseadmin/src/components/course/coursesPage.js
+++ reduxcourseadmin/src/components/course/coursesPage.js
@@ -1,8 +1,11 @@
 import React, {PropTypes} from 'react';
+import {connect} from 'react-redux';
+import * as courseActions from '../../actions/courseActions';
+import {bindActionCreators} from 'redux';
 
 class CoursesPage extends React.Component{
   constructor(props, context){
-    super(props,context)
+    super(props,context);
 
     this.state = {
       course: { title: ""}
@@ -19,13 +22,22 @@ class CoursesPage extends React.Component{
   }
 
   onClickSave(){
-    alert('Saving ${this.state.course.title}');
+    this.props.actions.createCourse(this.state.course);
+  }
+
+  courseRow(course,index){
+    return(
+      <div key={index}>
+        {course.title}
+      </div>
+    );
   }
 
   render(){
     return(
       <div>
         <h1>Courses</h1>
+        {this.props.courses.map(this.courseRow)}
         <h2>Add course</h2>
         <input
           type="text"
@@ -43,4 +55,21 @@ class CoursesPage extends React.Component{
   }
 }
 
-export default CoursesPage;
+function mapStateToProps(state, ownProps){
+  return ({
+    courses: state.courses
+  });
+}
+
+function mapDispatchToProps(dispatch){
+  return ({
+    actions: bindActionCreators(courseActions,dispatch)
+  });
+}
+
+CoursesPage.propTypes = {
+  actions: PropTypes.object.isRequired,
+  courses: PropTypes.array.isRequired
+};
+
+export default connect(mapStateToProps,mapDispatchToProps)(CoursesPage);
--- reduxcourseadmin/src/store/configureStore.js
+++ reduxcourseadmin/src/store/configureStore.js
@@ -1,6 +1,6 @@
 import {createStore, applyMiddleware} from 'redux';
-import rootReducer from '../reducers'
-import reduxImmutableStateInvariant from 'redux-immutable-state-invariant'
+import rootReducer from '../reducers';
+import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
 
 export default function configureStore(initialState){
   return createStore(
@@ -9,3 +9,4 @@ export default function configureStore(initialState){
     applyMiddleware(reduxImmutableStateInvariant())
   );
 }
+
