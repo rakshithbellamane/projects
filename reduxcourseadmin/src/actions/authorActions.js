77f29e466458a297cf1fca7d54f8b462bd1ba991
Rakshith Bellamane <rakshith.bellamane@libertymutual.com>
2018-11-14 08:29:04 -0500

    add form controls for manage course

---
+++ reduxcourseadmin/src/actions/authorActions.js
@@ -0,0 +1,20 @@
+import * as types from '../actions/actionTypes';
+import AuthorApi from '../api/mockAuthorApi';
+
+export function loadAuthorsSuccess(authors){
+  return {
+    type: types.LOAD_AUTHORS_SUCCESS,
+    authors
+  };
+}
+
+export function loadAuthors(){
+  return dispatch => {
+    return AuthorApi.getAllAuthors()
+      .then(
+        authors => {dispatch(loadAuthorsSuccess(authors));}
+    ).catch(error => {
+      throw(error);
+    });
+  };
+}
---
+++ reduxcourseadmin/src/components/course/ManageCoursePage.js
@@ -0,0 +1,69 @@
+import React,{PropTypes} from 'react';
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
+import * as courseActions from '../../actions/courseActions';
+import CourseForm from './CourseForm';
+
+
+class ManageCoursePage extends React.Component{
+  constructor(props,context){
+    super(props,context);
+
+    this.state = {
+      course: Object.assign({}, this.props.course),
+      errors: {}
+    };
+
+    this.updateCourseState = this.updateCourseState.bind(this);
+  }
+
+  updateCourseState(event){
+    const field = event.target.name;
+    let course = this.state.course;
+    course[field] = event.target.value;
+    return this.setState({course: course});
+  }
+
+  render(){
+    return(
+      <div>
+        <h1>Manage Course</h1>
+        <CourseForm
+          allAuthors={this.props.authors}
+          onChange={this.updateCourseState}
+          course={this.state.course}
+          errors={this.state.errors}
+        />
+      </div>
+    );
+  }
+}
+
+ManageCoursePage.propTypes = {
+  course: React.PropTypes.object.isRequired,
+  authors: React.PropTypes.array.isRequired
+};
+
+function mapStateToProps(state,ownProps){
+  let course = {id: '',watchHref:'', title:'', authorId:'', length:'', category:''};
+
+  const authorsFormattedForDropdown = state.authors.map(author => {
+    return {
+      value:author.id,
+      text:author.firstName + ' ' + author.lastName
+    };
+  });
+
+  return{
+    course: course,
+    authors: authorsFormattedForDropdown
+  };
+}
+
+function mapDispatchToProps(dispatch){
+  return {
+    actions: bindActionCreators(courseActions,dispatch)
+  };
+}
+
+export default connect(mapStateToProps,mapDispatchToProps)(ManageCoursePage);
